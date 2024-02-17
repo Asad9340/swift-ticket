09ad00231342d1c4b4a0a6e1198e7244db6f2f22
Asad9340 <asadulimran1999@gmail.com>
2024-02-18 04:26:44 +0600

    added some functionality

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+const btns = document.getElementsByClassName('btn');
+let btnClick = 0;
+let total = 0;
+for (const btn of btns) {
+  btn.addEventListener('click', e => {
+    colorBtn(e.target.id);
+    // totalCost.innerText = total + 550;
+  });
+}
+
+function colorBtn(btn) {
+  document.getElementById(btn).style.background = 'green';
+  const p = document.createElement('p');
+  p.textContent = btn;
+  const p2 = document.createElement('p');
+  p2.textContent = 'Economoy';
+  const p3 = document.createElement('p');
+  p3.textContent = 550;
+  let displayElement = document.getElementById('displayElement');
+  displayElement.appendChild(p);
+  displayElement.appendChild(p2);
+  displayElement.appendChild(p3);
+  document.getElementById(btn).disabled = true;
+}
